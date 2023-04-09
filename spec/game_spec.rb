bf9084b2b1db07fd51883be96082ca9952a1eae2
Bank Lee <iambanklee@gmail.com>
2023-04-09 11:11:59 +0100

    requirements

---
+++ spec/game_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.describe 'Game' do
+  describe 'A brief English sentence describes the room where you enter (ex. “You are in front of an old medieval castle…”)'
+  describe 'You can move between the rooms using some keyboard inputs'
+  describe 'There should be at least 4 different rooms'
+  describe 'There could be some events in the rooms'
+  describe 'You need to fight a final enemy'
+  describe 'A help section for the commands is necessary'
+end
