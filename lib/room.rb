c75d68180c5254daf42fb02982bf53a262b3e4a6
Bank Lee <iambanklee@gmail.com>
2023-04-11 23:55:25 +0100

    when entering a room, display message and brief description of the room

--- lib/room.rb
+++ lib/room.rb
@@ -3,11 +3,34 @@
 class Room
   attr_reader :name
 
+  ROOM_PREFIX_MODIFIERS = %w[old new terrible scary light warm].freeze
+  ROOM_MODIFIERS = %w[medieval cyberpunk stoneage darkside].freeze
+  ROOM_LOCATIONS = %w[castle dungeon room barrier].freeze
+
   def initialize(name:)
     @name = name
   end
 
   def enter
-    puts "room #{name}"
+    print "You entered the room #{name}."
+    puts description
+  end
+
+  def description
+    @description ||= "You are in a room look like #{room_prefix_modifier} #{room_modifier} #{room_location}"
+  end
+
+  private
+
+  def room_prefix_modifier
+    @room_prefix_modifier ||= ROOM_PREFIX_MODIFIERS.sample
+  end
+
+  def room_modifier
+    @room_modifier ||= ROOM_MODIFIERS.sample
+  end
+
+  def room_location
+    @room_location ||= ROOM_LOCATIONS.sample
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -4,8 +4,6 @@ require 'rspec'
 require 'room'
 
 RSpec.describe Room do
-  describe 'A brief English sentence describes the room where you enter (ex. “You are in front of an old medieval castle…”)'
-
   describe '.new' do
     subject(:new_room) { Room.new(name: name) }
 
@@ -13,6 +11,27 @@ RSpec.describe Room do
 
     it 'creates room by given name' do
       expect(new_room.name).to eq(name)
+    end
+
+    it 'sets room modifiers' do
+      expect(new_room.description).to match(Regexp.new(Room::ROOM_PREFIX_MODIFIERS.join('|')))
+      expect(new_room.description).to match(Regexp.new(Room::ROOM_MODIFIERS.join('|')))
+      expect(new_room.description).to match(Regexp.new(Room::ROOM_LOCATIONS.join('|')))
+    end
+  end
+
+  describe '#enter' do
+    subject(:enter_room) { room.enter }
+
+    let(:room){ Room.new(name: name) }
+    let(:name) { 'Room A' }
+
+    it 'output welcome message' do
+      expect { enter_room }.to output(/You entered the room #{name}/).to_stdout
+    end
+
+    it 'output room details' do
+      expect { enter_room }.to output(Regexp.new(room.description)).to_stdout
     end
   end
 
