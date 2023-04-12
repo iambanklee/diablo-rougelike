bc757ae06ab4a06e81b42f39b8603239b51147d2
Bank Lee <iambanklee@gmail.com>
2023-04-12 18:30:00 +0100

    Challenge solving flow

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -6,7 +6,7 @@ class Challenge
   def initialize
     @completed = false
   end
-  
+
   def formula
     @formula ||= "#{Random.rand(100)} #{CHALLENGE_OPERATORS.sample} #{Random.rand(100)}"
   end
@@ -17,5 +17,18 @@ class Challenge
 
   def completed?
     @completed
+  end
+
+  def start
+    until completed?
+      puts 'You need to solve this challenge before you can go anywhere'
+      puts formula
+      input = Kernel.gets.chomp
+
+      if input == expected_result.to_s
+        @completed = true
+        puts 'Challenge completed!'
+      end
+    end
   end
 end
--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -5,17 +5,43 @@ require 'challenge'
 
 RSpec.describe Challenge do
   describe '.new' do
-    subject(:new_Challenge) { Challenge.new }
+    subject(:new_challenge) { Challenge.new }
 
     it 'generate an random event' do
       allow(Random).to receive(:rand).and_return(50, 20)
       stub_const('Challenge::CHALLENGE_OPERATORS', ['-'])
 
-      event = new_Challenge
+      event = new_challenge
 
       expect(event.formula).to eq('50 - 20')
       expect(event.expected_result).to eq(30)
       expect(event.completed?).to eq(false)
+    end
+  end
+
+  describe '#start' do
+    subject(:challenge_start) { challenge.start }
+
+    let(:challenge) { Challenge.new }
+
+    context 'when challenge solved' do
+      before do
+        stub_const('Challenge::CHALLENGE_OPERATORS', ['*'])
+        allow(Random).to receive(:rand).and_return(5, 15)
+        allow(Kernel).to receive(:gets).and_return('100', '75')
+      end
+
+      it 'marks the challenge completed' do
+        expect{ challenge_start }.to output(<<~OUTPUT
+          You need to solve this challenge before you can go anywhere
+          5 * 15
+          You need to solve this challenge before you can go anywhere
+          5 * 15
+          Challenge completed!
+          OUTPUT
+        ).to_stdout
+        expect(challenge.completed?).to eq(true)
+      end
     end
   end
 end
